0d4974dbe94a3ec59a6690c7f4f51777566ac22e
cavera <cavera.de@gmail.com>
2023-09-27 21:37:22 -0500

    Update ENDPOINT to add search params

--- src/hooks/useQuestions.ts
+++ src/hooks/useQuestions.ts
@@ -3,12 +3,14 @@ import mockData from '../data/mockData.json'
 import { API_BASEURL } from '../data/consts'
 import { getData } from '../utils/getData'
 
-const QUESTIONS_ENDPOINT = 'https://opentdb.com/api.php?amount=10&type=multiple'
-
 export const useQuestions = () => {
   const [questions, setQuestions] = useState<Question[]>([])
   const [currentQIndex, setCurrentQIndex] = useState<number>(0)
   const [loading, setLoading] = useState(true)
+  const QUESTIONS_ENDPOINT = new URL(`${API_BASEURL}`)
+
+  QUESTIONS_ENDPOINT.searchParams.append('amount', '10')
+  QUESTIONS_ENDPOINT.searchParams.append('type', 'multiple')
 
   useEffect(() => {
     getQuestions()
@@ -16,7 +18,8 @@ export const useQuestions = () => {
 
   const getQuestions = async () => {
     try {
-      const data = await getData(QUESTIONS_ENDPOINT)
+      console.log(QUESTIONS_ENDPOINT.toString())
+      const data = await getData(QUESTIONS_ENDPOINT.toString())
 
       setQuestions(data.results)
       setLoading(false)
